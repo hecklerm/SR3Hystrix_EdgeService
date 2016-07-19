301a9a5a700ef7b198ce28ee0dacc91addec68d0
Mark Heckler <mark.heckler@gmail.com>
2016-07-18 19:59:31 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # SR3Hystrix_EdgeService
 Test project #2 for reproducing SC Brixton.SR3 issues with Hystrix.
+
+## Reproducing Issue
+
+. Run LOTE_ConfigService
+. Run LOTE_EurekaService
+. Run test project #1 (SR3Hystrix_QuoteService)
+. Run this project
+. Kill quote-service (test project #1 above, SR3Hystrix_QuoteService)
+. Update entry for quote in edge-service.properties (bootiful-microservices-config feeds the config-service)
+. git commit -a -m "Changed quote"
+. From terminal, curl -d{} localhost:8086/refresh
+. Verify updated quote provided by edge-service
+. Restart quote-service
+. If running SR3, wait as long as you like; the circuit never closes (at least it didn't on my machine)
+
+Changing the edge-service POM to use Brixton.RELEASE eliminates the issue. I haven't yet tested with SR1 or SR2.
+
+It's quite possible the config change & refresh contribute nothing to the discussion, but I included them for completeness, as these are the steps I was executing.
+
---
+++ src/main/java/com/example/EdgeServiceApplication.java
@@ -0,0 +1,92 @@
+package com.example;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+//@EnableBinding(Source.class)
+public class EdgeServiceApplication {
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+}
+
+@RestController
+@RefreshScope
+class QuoteController {
+//    @Autowired
+//    Source source;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${quote}")
+    private String defaultQuote;
+
+    @RequestMapping("/quotorama")
+    @HystrixCommand(fallbackMethod = "getDefaultQuote")
+    public Quote getRandomQuote() {
+        return restTemplate.getForObject("http://quote-service/random", Quote.class);
+    }
+
+    public Quote getDefaultQuote() {
+        return new Quote(defaultQuote, "Me");
+    }
+
+//    @RequestMapping(value="/newquote", method=RequestMethod.POST)
+//    public void acceptNewQuote(@RequestBody Quote quote) {
+//        this.source.output().send(MessageBuilder.withPayload(quote).build);
+//    }
+}
+
+class Quote {
+    private Long id;
+    private String text;
+    private String source;
+
+    public Quote() { //JSON
+    }
+
+    public Quote(String text, String source) {
+        this.text = text;
+        this.source = source;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", source='" + source + '\'' +
+                '}';
+    }
+}
